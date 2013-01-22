ff478b88f8a509b7350cdb2acd8ad4adcbd47d1d
Martin Cameron <mumart@gmail.com>
2013-01-22 19:47:05 +0000

    Add support for Webkit/Chrome Audio API. Thanks to Ben Lanier\!

--- micromod-js/AudioPlayer.js
+++ micromod-js/AudioPlayer.js
@@ -1,11 +1,42 @@
 
-function AudioPlayer( audioSource, latency ) {
+function createAudioPlayer( audioSource ) {
+	if( typeof( webkitAudioContext ) === "function" ) {
+		return new WebkitAudioPlayer( audioSource );
+	} else {
+		return new FirefoxAudioPlayer( audioSource );
+	}
+}
+
+function WebkitAudioPlayer( audioSource ) {
+	var audioContext = new webkitAudioContext();
+	var scriptProcessor = audioContext.createJavaScriptNode( 4096, 0, 2 );
+	var buffer = new Float32Array( scriptProcessor.bufferSize * 2 );
+	audioSource.setSamplingRate( audioContext.sampleRate );
+	scriptProcessor.onaudioprocess = function( event ) {
+		var lOut = event.outputBuffer.getChannelData( 0 );
+		var rOut = event.outputBuffer.getChannelData( 1 );
+		audioSource.getAudio( buffer, event.outputBuffer.length );
+		for( var bufIdx = 0, outIdx = 0; bufIdx < buffer.length; bufIdx += 2, outIdx++ ) {
+			lOut[ outIdx ] = buffer[ bufIdx ];
+			rOut[ outIdx ] = buffer[ bufIdx + 1 ];
+		}
+	}
+	this.play = function() {
+		scriptProcessor.connect( audioContext.destination );
+	}
+	this.stop = function() {
+		scriptProcessor.disconnect( audioContext.destination );
+	}
+}
+
+function FirefoxAudioPlayer( audioSource ) {
+	// 44100hz seems to sound best in Firefox.
+	audioSource.setSamplingRate( 44100 );
 	var samplingRate = audioSource.getSamplingRate();
-	var buffer = new Float32Array( samplingRate >> 1 /* 250ms */ );
+	var buffer = new Float32Array( samplingRate / 2 /* 250ms */ );
 	var bufferIndex = buffer.length, intervalId = null;
 	var audio = new Audio();
 	audio.mozSetup( 2, samplingRate );
-
 	this.play = function() {
 		if( intervalId == null ) {
 			var oldTime = new Date().getTime();
@@ -28,7 +59,6 @@ function AudioPlayer( audioSource, latency ) {
 			}, 125 );
 		}
 	}
-
 	this.stop = function() {
 		if( intervalId != null ) {
 			clearInterval( intervalId );
