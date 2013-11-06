6a512d99dd05c3a9efd40497b623e9bd2ebc9727
Martin Cameron <mumart@gmail.com>
2013-11-06 20:58:23 +0000

    Use the W3C Web Audio API.

--- micromod-js/AudioPlayer.js
+++ micromod-js/AudioPlayer.js
@@ -1,19 +1,10 @@
 
-function createAudioPlayer() {
-	if( typeof( AudioContext ) === "function" ) {
-		return new AudioPlayer( new AudioContext() );
-	} else if( typeof( webkitAudioContext ) === "function" ) {
-		return new AudioPlayer( new webkitAudioContext() );
-	} else {
-		return new FirefoxAudioPlayer();
-	}
-}
-
-function AudioPlayer( audioContext ) {
-	var scriptProcessor = audioContext.createJavaScriptNode( 4096, 0, 2 );
+function AudioPlayer() {
+	var audioContext = new AudioContext();
+	var scriptProcessor = audioContext.createScriptProcessor( 0, 0, 2 );
 	var buffer = new Float32Array( scriptProcessor.bufferSize * 2 );
 	var audioSource = new SineSource( audioContext.sampleRate );
-	scriptProcessor.onaudioprocess = function( event ) {
+	var onaudioprocess = function( event ) {
 		var lOut = event.outputBuffer.getChannelData( 0 );
 		var rOut = event.outputBuffer.getChannelData( 1 );
 		audioSource.getAudio( buffer, event.outputBuffer.length );
@@ -29,54 +20,12 @@ function AudioPlayer( audioContext ) {
 		audioSource = audioSrc;
 	}
 	this.play = function() {
+		scriptProcessor.onaudioprocess = onaudioprocess;
 		scriptProcessor.connect( audioContext.destination );
 	}
 	this.stop = function() {
 		scriptProcessor.disconnect( audioContext.destination );
-	}
-}
-
-function FirefoxAudioPlayer() {
-	// 44100hz seems to sound best in Firefox.
-	var samplingRate = 44100;
-	var buffer = new Float32Array( samplingRate >> 1 /* 250ms */ );
-	var bufferIndex = buffer.length, intervalId = null;
-	var audioSource = new SineSource( samplingRate );
-	var audio = new Audio();
-	audio.mozSetup( 2, samplingRate );
-	this.getSamplingRate = function() {
-		return samplingRate;
-	}
-	this.setAudioSource = function( audioSrc ) {
-		audioSource = audioSrc;
-	}
-	this.play = function() {
-		if( intervalId == null ) {
-			var oldTime = new Date().getTime();
-			intervalId = setInterval( function( a ) {
-				var newTime = new Date().getTime();
-				if( newTime - oldTime < 250 ) {
-					// Only write audio if the event came in roughly on time.
-					// Prevents stuttering when the script is running in the background.
-					if( bufferIndex < buffer.length ) {
-						// Finish writing current buffer.
-						bufferIndex += audio.mozWriteAudio( buffer.subarray( bufferIndex ) );
-					}
-					while( bufferIndex >= buffer.length ) {
-						// Write as many full buffers as possible.
-						audioSource.getAudio( buffer, buffer.length >> 1 );
-						bufferIndex = audio.mozWriteAudio( buffer );
-					}
-				}
-				oldTime = newTime;
-			}, 125 );
-		}
-	}
-	this.stop = function() {
-		if( intervalId != null ) {
-			clearInterval( intervalId );
-			intervalId = null;
-		}
+		scriptProcessor.onaudioprocess = null;
 	}
 }
 
