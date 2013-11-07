48ce4532ca2f5a2eef67d31f82e90353a9e983ca
Martin Cameron <mumart@gmail.com>
2013-11-07 21:48:41 +0000

    Improve antialiasing in sinc interpolator.

--- ibxm/Sample.java
+++ ibxm/Sample.java
@@ -14,8 +14,8 @@ public class Sample {
 	private int loopStart = 0, loopLength = 0;
 	private short[] sampleData;
 	
-	/* Constants for the 16-tap fixed-point sinc interpolator. */
-	private static final int LOG2_FILTER_TAPS = 4;
+	/* Constants for the fixed-point sinc interpolator. */
+	private static final int LOG2_FILTER_TAPS = 4; /* 16 taps. */
 	private static final int FILTER_TAPS = 1 << LOG2_FILTER_TAPS;
 	private static final int DELAY = FILTER_TAPS / 2;
 	private static final int LOG2_TABLE_ACCURACY = 4;
@@ -23,25 +23,44 @@ public class Sample {
 	private static final int TABLE_INTERP_SHIFT = FP_SHIFT - LOG2_TABLE_ACCURACY;
 	private static final int TABLE_INTERP_ONE = 1 << TABLE_INTERP_SHIFT;
 	private static final int TABLE_INTERP_MASK = TABLE_INTERP_ONE - 1;
-	private static final short[] SINC_TABLE = {
-		0,  0,   0,   0,    0,    0,     0, 32767,     0,     0,    0,    0,   0,   0,  0,  0,
-	   -1,  7, -31, 103, -279,  671, -1731, 32546,  2006,  -747,  312, -118,  37,  -8,  1,  0,
-	   -1, 12, -56, 190, -516, 1246, -3167, 31887,  4259, -1549,  648, -248,  78, -18,  2,  0,
-	   -1, 15, -74, 257, -707, 1714, -4299, 30808,  6722, -2375,  994, -384, 122, -29,  4,  0,
-	   -2, 17, -87, 305, -849, 2067, -5127, 29336,  9351, -3196, 1338, -520, 169, -41,  6,  0,
-	   -2, 18, -93, 334, -941, 2303, -5659, 27510, 12092, -3974, 1662, -652, 214, -53,  8,  0,
-	   -1, 17, -95, 346, -985, 2425, -5912, 25375, 14888, -4673, 1951, -771, 257, -65, 10,  0,
-	   -1, 16, -92, 341, -985, 2439, -5908, 22985, 17679, -5254, 2188, -871, 294, -76, 13, -1,
-	   -1, 15, -85, 323, -945, 2355, -5678, 20399, 20399, -5678, 2355, -945, 323, -85, 15, -1,
-	   -1, 13, -76, 294, -871, 2188, -5254, 17679, 22985, -5908, 2439, -985, 341, -92, 16, -1,
-		0, 10, -65, 257, -771, 1951, -4673, 14888, 25375, -5912, 2425, -985, 346, -95, 17, -1,
-		0,  8, -53, 214, -652, 1662, -3974, 12092, 27510, -5659, 2303, -941, 334, -93, 18, -2,
-		0,  6, -41, 169, -520, 1338, -3196,  9351, 29336, -5127, 2067, -849, 305, -87, 17, -2,
-		0,  4, -29, 122, -384,  994, -2375,  6722, 30808, -4299, 1714, -707, 257, -74, 15, -1,
-		0,  2, -18,  78, -248,  648, -1549,  4259, 31887, -3167, 1246, -516, 190, -56, 12, -1,
-		0,  1,  -8,  37, -118,  312,  -747,  2006, 32546, -1731,  671, -279, 103, -31,  7, -1,
-		0,  0,   0,   0,    0,    0,     0,     0, 32767,     0,    0,    0,   0,   0,  0,  0
-	};
+	private static final int LOG2_NUM_TABLES = LOG2_FILTER_TAPS - 1;
+	private static final int NUM_TABLES = 1 << LOG2_NUM_TABLES;
+	private static final short[][] SINC_TABLES = calculateSincTables();
+
+	private static short[][] calculateSincTables() {
+		short[][] sincTables = new short[ NUM_TABLES ][];
+		for( int tableIdx = 0; tableIdx < NUM_TABLES; tableIdx++ ) {
+			sincTables[ tableIdx ] = calculateSincTable( 1.0 / ( tableIdx + 1 ) );
+		}
+		return sincTables;
+	}
+
+	private static short[] calculateSincTable( double lowpass ) {
+		short[] sincTable = new short[ ( TABLE_ACCURACY + 1 ) * FILTER_TAPS ];
+		double windDT = -2.0 * Math.PI / FILTER_TAPS;
+		double sincDT = -Math.PI;
+		int tableIdx = 0;
+		for( int tableY = 0; tableY <= TABLE_ACCURACY; tableY++ ) {
+			double fracT = tableY / ( double ) TABLE_ACCURACY;
+			double sincT = Math.PI * ( FILTER_TAPS / 2 - 1 + fracT );
+			double windT = Math.PI + sincT * 2.0 / FILTER_TAPS;
+			for( int tableX = 0; tableX < FILTER_TAPS; tableX++ ) {
+				double sincY = lowpass;
+				if( sincT != 0 ) {
+					sincY = Math.sin( lowpass * sincT ) / sincT;
+				}
+				/* Blackman-Harris window function.*/
+				double windY = 0.35875;
+				windY -= 0.48829 * Math.cos( windT );
+				windY += 0.14128 * Math.cos( windT * 2 );
+				windY -= 0.01168 * Math.cos( windT * 3 );
+				sincTable[ tableIdx++ ] = ( short ) Math.round( sincY * windY * 32767 );
+				sincT += sincDT;
+				windT += windDT;
+			}
+		}
+		return sincTable;
+	}
 
 	public void setSampleData( short[] sampleData, int loopStart, int loopLength, boolean pingPong ) {
 		int sampleLength = sampleData.length;
@@ -125,6 +144,15 @@ public class Sample {
 
 	public void resampleSinc( int sampleIdx, int sampleFrac, int step,
 			int leftGain, int rightGain, int[] mixBuffer, int offset, int length ) {
+		int tableIdx = 0;
+		if( step > FP_ONE ) {
+			// Increase lowpass filter to avoid aliasing.
+			tableIdx = ( step >> FP_SHIFT ) - 1;
+			if( tableIdx >= NUM_TABLES ) {
+				tableIdx = NUM_TABLES - 1;
+			}
+		}
+		short[] sincTable = SINC_TABLES[ tableIdx ];
 		int loopLen = loopLength;
 		int loopEnd = loopStart + loopLen;
 		if( sampleIdx >= loopEnd )
@@ -137,40 +165,13 @@ public class Sample {
 				if( loopLen < 2 ) break;
 				while( sampleIdx >= loopEnd ) sampleIdx -= loopLen;
 			}
-			int tableIdx = ( sampleFrac >> TABLE_INTERP_SHIFT ) << LOG2_FILTER_TAPS;
+			int tableIdx1 = ( sampleFrac >> TABLE_INTERP_SHIFT ) << LOG2_FILTER_TAPS;
+			int tableIdx2 = tableIdx1 + FILTER_TAPS;
 			int a1 = 0, a2 = 0;
-			a1  = SINC_TABLE[ tableIdx + 0  ] * data[ sampleIdx + 0  ];
-			a1 += SINC_TABLE[ tableIdx + 1  ] * data[ sampleIdx + 1  ];
-			a1 += SINC_TABLE[ tableIdx + 2  ] * data[ sampleIdx + 2  ];
-			a1 += SINC_TABLE[ tableIdx + 3  ] * data[ sampleIdx + 3  ];
-			a1 += SINC_TABLE[ tableIdx + 4  ] * data[ sampleIdx + 4  ];
-			a1 += SINC_TABLE[ tableIdx + 5  ] * data[ sampleIdx + 5  ];
-			a1 += SINC_TABLE[ tableIdx + 6  ] * data[ sampleIdx + 6  ];
-			a1 += SINC_TABLE[ tableIdx + 7  ] * data[ sampleIdx + 7  ];
-			a1 += SINC_TABLE[ tableIdx + 8  ] * data[ sampleIdx + 8  ];
-			a1 += SINC_TABLE[ tableIdx + 9  ] * data[ sampleIdx + 9  ];
-			a1 += SINC_TABLE[ tableIdx + 10 ] * data[ sampleIdx + 10 ];
-			a1 += SINC_TABLE[ tableIdx + 11 ] * data[ sampleIdx + 11 ];
-			a1 += SINC_TABLE[ tableIdx + 12 ] * data[ sampleIdx + 12 ];
-			a1 += SINC_TABLE[ tableIdx + 13 ] * data[ sampleIdx + 13 ];
-			a1 += SINC_TABLE[ tableIdx + 14 ] * data[ sampleIdx + 14 ];
-			a1 += SINC_TABLE[ tableIdx + 15 ] * data[ sampleIdx + 15 ];
-			a2  = SINC_TABLE[ tableIdx + 16 ] * data[ sampleIdx + 0  ];
-			a2 += SINC_TABLE[ tableIdx + 17 ] * data[ sampleIdx + 1  ];
-			a2 += SINC_TABLE[ tableIdx + 18 ] * data[ sampleIdx + 2  ];
-			a2 += SINC_TABLE[ tableIdx + 19 ] * data[ sampleIdx + 3  ];
-			a2 += SINC_TABLE[ tableIdx + 20 ] * data[ sampleIdx + 4  ];
-			a2 += SINC_TABLE[ tableIdx + 21 ] * data[ sampleIdx + 5  ];
-			a2 += SINC_TABLE[ tableIdx + 22 ] * data[ sampleIdx + 6  ];
-			a2 += SINC_TABLE[ tableIdx + 23 ] * data[ sampleIdx + 7  ];
-			a2 += SINC_TABLE[ tableIdx + 24 ] * data[ sampleIdx + 8  ];
-			a2 += SINC_TABLE[ tableIdx + 25 ] * data[ sampleIdx + 9  ];
-			a2 += SINC_TABLE[ tableIdx + 26 ] * data[ sampleIdx + 10 ];
-			a2 += SINC_TABLE[ tableIdx + 27 ] * data[ sampleIdx + 11 ];
-			a2 += SINC_TABLE[ tableIdx + 28 ] * data[ sampleIdx + 12 ];
-			a2 += SINC_TABLE[ tableIdx + 29 ] * data[ sampleIdx + 13 ];
-			a2 += SINC_TABLE[ tableIdx + 30 ] * data[ sampleIdx + 14 ];
-			a2 += SINC_TABLE[ tableIdx + 31 ] * data[ sampleIdx + 15 ];
+			for( int tap = 0; tap < FILTER_TAPS; tap++ ) {
+				a1 += sincTable[ tableIdx1 + tap ] * data[ sampleIdx + tap ];
+				a2 += sincTable[ tableIdx2 + tap ] * data[ sampleIdx + tap ];
+			}
 			a1 >>= FP_SHIFT;
 			a2 >>= FP_SHIFT;
 			int y = a1 + ( ( a2 - a1 ) * ( sampleFrac & TABLE_INTERP_MASK ) >> TABLE_INTERP_SHIFT );
