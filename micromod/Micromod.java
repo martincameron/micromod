04c8a417d86c568806fb69a44da5c1c0a8f73e18
Martin Cameron <mumart@gmail.com>
2014-03-12 22:13:33 +0000

    Update version.

--- micromod/Micromod.java
+++ micromod/Micromod.java
@@ -5,7 +5,7 @@ package micromod;
 	Java ProTracker Replay (c)2014 mumart@gmail.com
 */
 public class Micromod {
-	public static final String VERSION = "20140310 (c)2014 mumart@gmail.com";
+	public static final String VERSION = "20140312 (c)2014 mumart@gmail.com";
 
 	private Module module;
 	private int[] rampBuf;
