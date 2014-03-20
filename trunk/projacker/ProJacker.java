035a4a646d69896d93328b9aa316b766742242c5
mumart@gmail.com <mumart@gmail.com@6b089e85-8945-e4b7-332b-c64baffd5526>
2014-03-20 23:49:48 +0000

    Make Player more reusable.

--- trunk/projacker/ProJacker.java
+++ trunk/projacker/ProJacker.java
@@ -3,46 +3,6 @@ package projacker;
 
 /* A simple text format for Protracker MOD files. */
 public class ProJacker {
-	public static void play( micromod.Module module ) throws javax.sound.sampled.LineUnavailableException {
-		final int SAMPLE_RATE = 48000;
-		// Initialise Micromod.
-		micromod.Micromod micromod = new micromod.Micromod( module, SAMPLE_RATE );
-		//micromod.setInterpolation( true );
-		// Print some info.
-		System.out.println( "Micromod " + micromod.VERSION );
-		System.out.println( "Song name: " + module.getSongName() );
-		for( int idx = 1; idx < 32; idx++ ) {
-			String name = module.getInstrument( idx ).getName();
-			if( name != null && name.trim().length() > 0 )
-				System.out.println( String.format( "%1$3d ", idx ) + name );
-		}
-		int duration = micromod.calculateSongDuration();
-		System.out.println( "Song length: " + duration / SAMPLE_RATE + " seconds." );
-		// Application will exit when song finishes.
-		int[] mixBuf = new int[ micromod.getMixBufferLength() ];
-		byte[] outBuf = new byte[ mixBuf.length * 4 ];
-		javax.sound.sampled.AudioFormat audioFormat = new javax.sound.sampled.AudioFormat( SAMPLE_RATE, 16, 2, true, true );
-		javax.sound.sampled.SourceDataLine audioLine = javax.sound.sampled.AudioSystem.getSourceDataLine( audioFormat );
-		audioLine.open();
-		audioLine.start();
-		int samplePos = 0;
-		while( samplePos < duration ) {
-			int count = micromod.getAudio( mixBuf );
-			int outIdx = 0;
-			for( int mixIdx = 0, mixEnd = count * 2; mixIdx < mixEnd; mixIdx++ ) {
-				int sam = mixBuf[ mixIdx ];
-				if( sam > 32767 ) sam = 32767;
-				if( sam < -32768 ) sam = -32768;
-				outBuf[ outIdx++ ] = ( byte ) ( sam >> 8 );
-				outBuf[ outIdx++ ] = ( byte )  sam;
-			}
-			audioLine.write( outBuf, 0, outIdx );
-			samplePos += count;
-		}
-		audioLine.drain();
-		audioLine.close();
-	}
-
 	public static void main( String[] args ) throws Exception {
 		if( args.length > 0 ) {
 			Module module = new Module();
@@ -55,7 +15,11 @@ public class ProJacker {
 					outputStream.close();
 				}
 			} else {
-				play( module.getModule() );
+				micromod.Player player = new micromod.Player( module.getModule(), false, false );
+				System.out.println( player.getModuleInfo() );
+				Thread thread = new Thread( player );
+				thread.start();
+				thread.join();
 			}
 		} else {
 			System.err.println( "Usage java " + ProJacker.class.getName() + " input.pj output.mod" );
