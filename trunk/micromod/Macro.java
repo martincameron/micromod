e1ac2e7ef3a4cfb811801d5b8668c22cb7c9274b
mumart <mumart@6b089e85-8945-e4b7-332b-c64baffd5526>
2014-04-12 00:37:19 +0000

    Make transpose more robust.

--- trunk/micromod/Macro.java
+++ trunk/micromod/Macro.java
@@ -28,26 +28,30 @@ public class Macro {
 			int effect = note.effect;
 			int param = note.parameter;
 			notes.getNote( macroRowIdx++, 0, note );
+			if( note.key > 0 ) {
+				srcKey = note.key;
+				dstKey = scale.transpose( srcKey, distance );
+			}
+			int semitones = 0;
+			if( dstKey > 0 ) {
+				semitones = dstKey - srcKey;
+			}
 			if( effect == 0xC ) {
 				volume = param;
 			} else if( ( note.effect | note.parameter ) == 0 ) {
 				note.effect = effect;
 				note.parameter = param;
 			}
-			int semitones = 0;
-			if( note.key > 0 ) {
-				srcKey = note.key;
-				dstKey = scale.transpose( srcKey, distance );
-				semitones = dstKey - srcKey;
-			}
 			note.transpose( semitones, volume, module );
-			if( srcKey > 0 && note.effect == 0 && note.parameter != 0 ) {
+			if( semitones != 0 && note.effect == 0 && note.parameter != 0 ) {
 				/* Adjust arpeggio.*/
 				int dist = scale.getDistance( srcKey, srcKey + ( ( note.parameter >> 4 ) & 0xF ) );
-				int arp1 = scale.transpose( srcKey, distance + dist ) - dstKey;
+				int arp1 = scale.transpose( dstKey, dist ) - dstKey;
+				if( arp1 < 0 ) arp1 = 0;
 				if( arp1 > 15 ) arp1 = ( arp1 - 3 ) % 12 + 3;
 				dist = scale.getDistance( srcKey, srcKey + ( note.parameter & 0xF ) );
-				int arp2 = scale.transpose( srcKey, distance + dist ) - dstKey;
+				int arp2 = scale.transpose( dstKey, dist ) - dstKey;
+				if( arp2 < 0 ) arp2 = 0;
 				if( arp2 > 15 ) arp2 = ( arp2 - 3 ) % 12 + 3;
 				note.parameter = ( arp1 << 4 ) + ( arp2 & 0xF );
 			}
