07bb5db33b08dbe0f39bee960c0e0150798817e3
mumart <mumart@6b089e85-8945-e4b7-332b-c64baffd5526>
2014-10-22 21:32:13 +0000

    Add wave cropping and sample offset based time-stretching to tracker.

--- trunk/micromod/tracker/Note.java
+++ trunk/micromod/tracker/Note.java
@@ -3,7 +3,9 @@ package micromod.tracker;
 
 public class Note implements Element {
 	private Macro parent;
+	private TimeStretch child = new TimeStretch( this );
 	private micromod.Note note = new micromod.Note();
+	private int timeStretchRows;
 	
 	public Note( Macro parent ) {
 		this.parent = parent;
@@ -22,14 +24,29 @@ public class Note implements Element {
 	}
 	
 	public Element getChild() {
-		return null;
+		return child;
 	}
 	
 	public void begin( String value ) {
 		note.fromString( value );
-		parent.nextNote( note );
+		timeStretchRows = 0;
 	}
 	
 	public void end() {
-	}	
+		parent.nextNote( note );
+		if( timeStretchRows > 1 ) {
+			micromod.Instrument instrument = parent.getModule().getInstrument( note.instrument );
+			int sampleLength = instrument.getLoopStart() + instrument.getLoopLength();
+			note.effect = 0x9;
+			for( int row = 1; row < timeStretchRows; row++ ) {
+				int offset = ( sampleLength * row ) / ( timeStretchRows << 7 );
+				note.parameter = ( offset >> 1 ) + ( offset & 1 );
+				parent.nextNote( note );
+			}
+		}
+	}
+
+	public void setTimeStretch( int rows ) {
+		timeStretchRows = rows;
+	}
 }
