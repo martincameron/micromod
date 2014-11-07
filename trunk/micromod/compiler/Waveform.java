2d1ae9018244bad0709b7e565d91dd5982ce20c9
mumart <mumart@6b089e85-8945-e4b7-332b-c64baffd5526>
2014-11-07 22:48:41 +0000

    Add simple noise synthesis.

--- trunk/micromod/compiler/Waveform.java
+++ trunk/micromod/compiler/Waveform.java
@@ -6,7 +6,7 @@ public class Waveform implements Element {
 	private Octave child = new Octave( this );
 	private byte[] envelope = new byte[ 512 ];
 	private int octave, detune, chorus, x0, y0;
-	private boolean spectral;
+	private boolean spectral, noise;
 
 	public Waveform( Instrument parent ) {
 		this.parent = parent;
@@ -31,6 +31,7 @@ public class Waveform implements Element {
 	
 	public void begin( String value ) {
 		if( "Sine".equals( value ) || "Harmonics".equals( value ) ) {
+			noise = false;
 			spectral = true;
 			setEnvelopePoint(   0,    0 );
 			setEnvelopePoint(   1, -128 );
@@ -38,18 +39,25 @@ public class Waveform implements Element {
 			setEnvelopePoint( 511,    0 );
 			setEnvelopePoint(   0,    0 );
 		} else if( "Sawtooth".equals( value ) || "Envelope".equals( value ) ) {
-			spectral = false;
+			spectral = noise = false;
 			setEnvelopePoint(   0,    0 );
 			setEnvelopePoint( 255,  127 );
 			setEnvelopePoint( 256, -128 );
 			setEnvelopePoint( 511,   -1 );
 			setEnvelopePoint(   0,    0 );
 		} else if( "Square".equals( value ) ) {
-			spectral = false;
+			spectral = noise = false;
 			setEnvelopePoint(   0, -128 );
 			setEnvelopePoint( 255, -128 );
 			setEnvelopePoint( 256,  127 );
 			setEnvelopePoint( 511,  127 );
+		} else if( "Noise".equals( value ) ) {
+			spectral = noise = true;
+			setEnvelopePoint( 0, 0 );
+			setEnvelopePoint( 1, 1 );
+			setEnvelopePoint( 256, 1 );
+			setEnvelopePoint( 511, 0 );
+			setEnvelopePoint( 0, 0 );
 		} else {
 			throw new IllegalArgumentException( "Invalid waveform type: " + value );
 		}
@@ -59,14 +67,18 @@ public class Waveform implements Element {
 	}
 
 	public void end() {
-		int cycles = 1;
-		if( chorus > 1 ) {
-			cycles = chorus;
-		} else if( detune != 0 ) {
-			cycles = 128;
+		if( noise ) {
+			parent.setAudioData( new AudioData( noise( envelope ), 48000 ) );
+		} else {
+			int cycles = 1;
+			if( chorus > 1 ) {
+				cycles = chorus;
+			} else if( detune != 0 ) {
+				cycles = 128;
+			}
+			byte[] waveform = spectral ? harmonics( envelope ) : envelope;
+			parent.setAudioData( generate( waveform, cycles, octave, detune, chorus > 1 ) );
 		}
-		byte[] waveform = spectral ? harmonics( envelope ) : envelope;
-		parent.setAudioData( generate( waveform, cycles, octave, detune, chorus > 1 ) );
 		parent.setLoopStart( 0 );
 		parent.setLoopLength( parent.getAudioData().getLength() );
 	}
@@ -133,10 +145,7 @@ public class Waveform implements Element {
 
 	/* Generate a 512-byte periodic waveform from the specified 256-harmonic spectrum. */
 	public static byte[] harmonics( byte[] spectrum ) {
-		byte[] sine = new byte[ 512 ];
-		for( int idx = 0; idx < 512; idx++ ) {
-			sine[ idx ] = ( byte ) Math.round( Math.sin( Math.PI * idx / 256 ) * 127 );
-		}
+		byte[] sine = sine();
 		byte[] wave = new byte[ 512 ];
 		for( int idx = 0; idx < 512; idx++ ) {
 			int amp = 0;
@@ -148,5 +157,42 @@ public class Waveform implements Element {
 			wave[ idx ] = ( byte ) ( amp / 128 );
 		}
 		return wave;
+	}
+
+	/* Generate a 65536-byte noise waveform from the specified 256-harmonic spectrum. */
+	public static byte[] noise( byte[] spectrum ) {
+		byte[] sine = sine();
+		int[] wave = new int[ 512 ];
+		byte[] out = new byte[ 65536 ];
+		int random = 0;
+		for( int offset = 0; offset < 65536; offset += 256 ) {
+			for( int idx = 0; idx < 512; idx++ ) {
+				wave[ idx ] = 0;
+			}
+			for( int partial = 1; partial <= 256; partial++ ) {
+				random = random * 65 + 17;
+				for( int idx = 0; idx < 512; idx++ ) {
+					wave[ idx ] += sine[ ( ( random >> 24 ) + idx * partial ) & 0x1FF ] * spectrum[ partial ];
+				}
+			}
+			for( int idx = 0; idx < 512; idx++ ) {
+				if( wave[ idx ] < -16384 ) wave[ idx ] = -16384;
+				if( wave[ idx ] >  16383 ) wave[ idx ] =  16383;
+			}
+			for( int idx = 0; idx < 256; idx++ ) {
+				out[ idx + offset ] += idx * wave[ idx ] / 32768;
+				out[ ( idx + offset + 256 ) & 0xFFFF ] += ( 256 - idx ) * wave[ idx + 256 ] / 32768;
+			}
+		}
+		return out;
+	}
+
+	/* Generate a 512-byte sine table. */
+	public static byte[] sine() {
+		byte[] sine = new byte[ 512 ];
+		for( int idx = 0; idx < 512; idx++ ) {
+			sine[ idx ] = ( byte ) Math.round( Math.sin( Math.PI * idx / 256 ) * 127 );
+		}
+		return sine;
 	}
 }
