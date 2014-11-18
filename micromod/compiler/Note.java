6d4ba89a8a61a6eef5c8244c9465bfa309e8d47e
Martin Cameron <mumart@gmail.com>
2014-11-18 23:36:31 +0000

    Replace fade macro with linear volume slide.

--- micromod/compiler/Note.java
+++ micromod/compiler/Note.java
@@ -5,7 +5,7 @@ public class Note implements Element {
 	private Macro parent;
 	private TimeStretch child = new TimeStretch( this );
 	private micromod.Note note = new micromod.Note();
-	private int fade, repeat, timeStretch;
+	private int repeat, timeStretch;
 
 	public Note( Macro parent ) {
 		this.parent = parent;
@@ -28,45 +28,19 @@ public class Note implements Element {
 	}
 	
 	public void begin( String value ) {
-		fade = repeat = timeStretch = 0;
+		repeat = timeStretch = 0;
 		note.fromString( value );
-		if( note.effect == 0xC && note.parameter > 0x40 ) {
-			/* Apply x^2 volume-curve for effect C41 to C4F. */
-			int vol = ( note.parameter & 0xF ) + 1;
-			note.parameter = ( vol * vol ) >> 2;
-		}
 	}
 
 	public void end() {
-		parent.nextNote( note, fade, repeat, timeStretch );
+		parent.nextNote( note, repeat, timeStretch );
 	}
 
 	public void setTimeStretch( int rows ) {
 		timeStretch = rows;
 	}
 
-	public void beginEffect( String token ) {
-		if( "Repeat".equals( token ) ) {
-			repeat = Macro.BEGIN;
-		} else if( "Fade".equals( token ) ) {
-			fade = Macro.BEGIN;
-		} else {
-			throw new IllegalArgumentException( "Unknown command: Begin " + token );
-		}
-	}
-
-	public void endEffect( String token ) {
-		if( "Fade".equals( token ) ) {
-			fade = Macro.END;
-		} else {
-			throw new IllegalArgumentException( "Unknown command: End " + token );
-		}
-	}
-
-	public void endRepeat( int count ) {
-		if( count < 2 ) {
-			throw new IllegalArgumentException( "Invalid repeat count (2 or more): " + count );
-		}
+	public void setRepeat( int count ) {
 		repeat = count;
 	}
 }
