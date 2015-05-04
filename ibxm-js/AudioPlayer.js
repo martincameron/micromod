bec1eb1417b493095f525fc41f97effa8b84889b
martincameron <mumart@gmail.com>
2015-05-04 22:31:40 +0100

    Use floating-point mixer and frequency calculation.

--- ibxm-js/AudioPlayer.js
+++ ibxm-js/AudioPlayer.js
@@ -2,16 +2,11 @@
 function AudioPlayer() {
 	var audioContext = new AudioContext();
 	var scriptProcessor = audioContext.createScriptProcessor( 0, 0, 2 );
-	var buffer = new Float32Array( scriptProcessor.bufferSize * 2 );
 	var audioSource = new SineSource( audioContext.sampleRate );
 	var onaudioprocess = function( event ) {
-		var lOut = event.outputBuffer.getChannelData( 0 );
-		var rOut = event.outputBuffer.getChannelData( 1 );
-		audioSource.getAudio( buffer, event.outputBuffer.length );
-		for( var bufIdx = 0, outIdx = 0; bufIdx < buffer.length; bufIdx += 2, outIdx++ ) {
-			lOut[ outIdx ] = buffer[ bufIdx ];
-			rOut[ outIdx ] = buffer[ bufIdx + 1 ];
-		}
+		var leftBuf = event.outputBuffer.getChannelData( 0 );
+		var rightBuf = event.outputBuffer.getChannelData( 1 );
+		audioSource.getAudio( leftBuf, rightBuf, event.outputBuffer.length );
 	}
 	this.getSamplingRate = function() {
 		return audioContext.sampleRate;
@@ -31,17 +26,16 @@ function AudioPlayer() {
 
 function SineSource( samplingRate ) {
 	// Simple AudioSource for testing.
-	this.getSamplingRate = function() {
-		return rate;
-	}
-	this.getAudio = function( buffer, count ) {
-		for( idx = 0, end = count * 2; idx < end; idx += 2, phase++ ) {
-			var x = phase * freq;
-			buffer[ idx ] = Math.sin( x );
-			buffer[ idx + 1 ] = Math.sin( x * 0.5 );
-		}
-	}
 	var rate = samplingRate;
 	var freq = 2 * Math.PI * 440 / rate;
 	var phase = 0;
+	this.getSamplingRate = function() {
+		return rate;
+	}
+	this.getAudio = function( leftBuffer, rightBuffer, count ) {
+		for( var idx = 0; idx < count; idx++, phase++ ) {
+			leftBuffer[ idx ] = Math.sin( phase * freq );
+			rightBuffer[ idx ] = Math.sin( phase * freq * 0.5 );
+		}
+	}
 }
--- ibxm-js/IBXM.js
+++ ibxm-js/IBXM.js
@@ -2,7 +2,7 @@
 function IBXMReplay( module, samplingRate ) {
 	/* Return a String representing the version of the replay. */
 	this.getVersion = function() {
-		return "20150502 (c)2015 mumart@gmail.com";
+		return "20150504 (c)2015 mumart@gmail.com";
 	}
 	/* Return the sampling rate of playback. */
 	this.getSamplingRate = function() {
@@ -10,8 +10,8 @@ function IBXMReplay( module, samplingRate ) {
 	}
 	/* Set the sampling rate of playback. */
 	this.setSamplingRate = function( rate ) {
-		// Use with Module.c2Rate to adjust the tempo of playback.
-		// To play at half speed, multiply both the samplingRate and Module.c2Rate by 2.
+		/* Use with Module.c2Rate to adjust the tempo of playback.*/
+		/* To play at half speed, multiply both the samplingRate and Module.c2Rate by 2.*/
 		if( rate < 8000 || rate > 128000 ) {
 			throw "Unsupported sampling rate!";
 		}
@@ -93,70 +93,68 @@ function IBXMReplay( module, samplingRate ) {
 			for( var idx = 0; idx < module.numChannels; idx++ ) {
 				channels[ idx ].updateSampleIdx( tickLen * 2, sampleRate * 2 );
 			}
-			if( seqTick() ) { // Song end reached.
+			if( seqTick() ) { /* Song end reached.*/
 				setSequencePos( sequencePos );
 				return;
 			}
 		}
 	}
-	/* Write count floating-point stereo samples into outputBuf. */
-	this.getAudio = function( outputBuf, count ) {
+	/* Write count floating-point stereo samples into the specified buffers. */
+	this.getAudio = function( leftBuf, rightBuf, count ) {
 		var outIdx = 0;
 		while( outIdx < count ) {
 			if( mixIdx >= mixLen ) {
-				mixLen = mixAudio( mixBuf );
+				mixLen = mixAudio();
 				mixIdx = 0;
 			}
 			var remain = mixLen - mixIdx;
 			if( ( outIdx + remain ) > count ) {
 				remain = count - outIdx;
 			}
-			for( var idx = outIdx << 1, end = ( outIdx + remain ) << 1, mix = mixIdx << 1; idx < end; ) {
-				// Convert to floating-point and divide by ~32768 for output.
-				outputBuf[ idx++ ] = mixBuf[ mix++ ] * 0.0000305;
+			for( var end = outIdx + remain; outIdx < end; outIdx++, mixIdx++ ) {
+				leftBuf[ outIdx ] = mixBuf[ mixIdx * 2 ];
+				rightBuf[ outIdx ] = mixBuf[ mixIdx * 2 + 1 ];
 			}
-			mixIdx += remain;
-			outIdx += remain;
 		}
 	}
-	var mixAudio = function( outputBuf ) {
-		// Generate audio. The number of samples produced is returned.
+	var mixAudio = function() {
+		/* Generate audio. The number of samples produced is returned.*/
 		var tickLen = calculateTickLen( tempo, samplingRate );
 		for( var idx = 0, end = ( tickLen + 65 ) * 4; idx < end; idx++ ) {
-			// Clear mix buffer.
+			/* Clear mix buffer.*/
 			mixBuf[ idx ] = 0;
 		}
 		for( var idx = 0; idx < module.numChannels; idx++ ) {
-			// Resample and mix each channel.
+			/* Resample and mix each channel.*/
 			var chan = channels[ idx ];
 			chan.resample( mixBuf, 0, ( tickLen + 65 ) * 2, samplingRate * 2, interpolation );
 			chan.updateSampleIdx( tickLen * 2, samplingRate * 2 );
 		}
 		downsample( mixBuf, tickLen + 64 );
-		volumeRamp( mixBuf, tickLen );
-		// Update the sequencer.
+		volumeRamp( tickLen );
+		/* Update the sequencer.*/
 		seqTick();
 		return tickLen;
 	}
 	var calculateTickLen = function( tempo, sampleRate ) {
 		return ( ( sampleRate * 5 ) / ( tempo * 2 ) ) | 0;
 	}
-	var volumeRamp = function( mixBuf, tickLen ) {
-		var rampRate = ( 256 * 2048 / samplingRate ) | 0;
-		for( var idx = 0, a1 = 0; a1 < 256; idx += 2, a1 += rampRate ) {
-			var a2 = 256 - a1;
-			mixBuf[ idx     ] = ( mixBuf[ idx     ] * a1 + rampBuf[ idx     ] * a2 ) >> 8;
-			mixBuf[ idx + 1 ] = ( mixBuf[ idx + 1 ] * a1 + rampBuf[ idx + 1 ] * a2 ) >> 8;
+	var volumeRamp = function( tickLen ) {
+		var rampRate = 2048 / samplingRate;
+		for( var idx = 0, a1 = 0; a1 < 1; idx += 2, a1 += rampRate ) {
+			var a2 = 1 - a1;
+			mixBuf[ idx     ] = mixBuf[ idx     ] * a1 + rampBuf[ idx     ] * a2;
+			mixBuf[ idx + 1 ] = mixBuf[ idx + 1 ] * a1 + rampBuf[ idx + 1 ] * a2;
 		}
 		rampBuf.set( mixBuf.subarray( tickLen * 2, ( tickLen + 64 ) * 2 ) );
 	}
 	var downsample = function( buf, count ) {
-		// 2:1 downsampling with simple but effective anti-aliasing.
-		// Buf must contain count * 2 + 1 stereo samples.
+		/* 2:1 downsampling with simple but effective anti-aliasing.*/
+		/* Buf must contain count * 2 + 1 stereo samples.*/
 		var outLen = count * 2;
 		for( inIdx = 0, outIdx = 0; outIdx < outLen; inIdx += 4, outIdx += 2 ) {
-			buf[ outIdx     ] = ( buf[ inIdx     ] >> 2 ) + ( buf[ inIdx + 2 ] >> 1 ) + ( buf[ inIdx + 4 ] >> 2 );
-			buf[ outIdx + 1 ] = ( buf[ inIdx + 1 ] >> 2 ) + ( buf[ inIdx + 3 ] >> 1 ) + ( buf[ inIdx + 5 ] >> 2 );
+			buf[ outIdx     ] = buf[ inIdx     ] * 0.25 + buf[ inIdx + 2 ] * 0.5 + buf[ inIdx + 4 ] * 0.25;
+			buf[ outIdx + 1 ] = buf[ inIdx + 1 ] * 0.25 + buf[ inIdx + 3 ] * 0.5 + buf[ inIdx + 5 ] * 0.25;
 		}
 	}
 	var seqTick = function() {
@@ -264,9 +262,9 @@ function IBXMReplay( module, samplingRate ) {
 		}
 		return songEnd;
 	}
-	var interpolation = false;
-	var rampBuf = new Int32Array( 64 * 2 );
-	var mixBuf = new Int32Array( ( calculateTickLen( 32, 128000 ) + 65 ) * 4 );
+	var interpolation = true;
+	var rampBuf = new Float32Array( 64 * 2 );
+	var mixBuf = new Float32Array( ( calculateTickLen( 32, 128000 ) + 65 ) * 4 );
 	var mixIdx = 0, mixLen = 0;
 	var seqPos = 0, breakSeqPos = 0, row = 0, nextRow = 0, tick = 0;
 	var speed = 0, tempo = 0, plCount = 0, plChannel = 0;
@@ -279,11 +277,15 @@ function IBXMReplay( module, samplingRate ) {
 }
 
 function IBXMChannel( replay, id ) {
+	var sineTable = new Int16Array([
+		   0,  24,  49,  74,  97, 120, 141, 161, 180, 197, 212, 224, 235, 244, 250, 253,
+		 255, 253, 250, 244, 235, 224, 212, 197, 180, 161, 141, 120,  97,  74,  49,  24
+	]);
 	var instrument = new IBXMInstrument();
 	var sample = instrument.samples[ 0 ];
 	var keyOn = false;
 	var noteKey = 0, noteIns = 0, noteVol = 0, noteEffect = 0, noteParam = 0;
-	var sampleOffset = 0, sampleIdx = 0, sampleFra = 0, freq = 0, ampl = 0, pann = 0;
+	var sampleOffset = 0, sampleIdx = 0, freq = 0, ampl = 0, pann = 0;
 	var volume = 0, panning = replay.module.defaultPanning[ id ];
 	var fadeOutVol = 0, volEnvTick = 0, panEnvTick = 0;
 	var period = 0, portaPeriod = 0, retrigCount = 0, fxCount = 0, autoVibratoCount = 0;
@@ -295,61 +297,55 @@ function IBXMChannel( replay, id ) {
 	var vibratoType = 0, vibratoPhase = 0, vibratoSpeed = 0, vibratoDepth = 0;
 	var tremoloType = 0, tremoloPhase = 0, tremoloSpeed = 0, tremoloDepth = 0;
 	var tremoloAdd = 0, vibratoAdd = 0, arpeggioAdd = 0;
+	var randomSeed = ( id + 1 ) * 0xABCDEF;
 	this.plRow = 0;
-	this.resample = function( mixBuffer, offset, count, sampleRate, interpolate ) {
+	this.resample = function( outBuf, offset, count, sampleRate, interpolate ) {
 		if( ampl <= 0 ) return;
-		var lGain = ampl * ( 255 - pann ) >> 8;
-		var rGain = ampl * pann >> 8;
-		var step = ( ( freq << ( 0xF - 3 ) ) / ( sampleRate >> 3 ) ) | 0;
-		var samIdx = sampleIdx | 0;
-		var samFra = sampleFra | 0;
-		var loopLen = sample.loopLength | 0;
-		var loopEnd = ( sample.loopStart + loopLen ) | 0;
+		var lGain = ampl * ( 1 - pann ) / 32768;
+		var rGain = ampl * pann / 32768;
+		var samIdx = sampleIdx;
+		var step = freq / sampleRate;
+		var loopLen = sample.loopLength;
+		var loopEnd = sample.loopStart + loopLen;
 		var sampleData = sample.sampleData;
-		var outIdx = offset << 1;
-		var outEnd = ( offset + count ) << 1;
+		var outIdx = offset * 2;
+		var outEnd = ( offset + count ) * 2;
 		if( interpolate ) {
 			while( outIdx < outEnd ) {
 				if( samIdx >= loopEnd ) {
-					if( loopLen < 2 ) break;
+					if( loopLen <= 1 ) break;
 					while( samIdx >= loopEnd ) samIdx -= loopLen;
 				}
-				var c = sampleData[ samIdx ];
-				var m = sampleData[ samIdx + 1 ] - c;
-				var y = ( m * samFra >> 0xF ) + c;
-				mixBuffer[ outIdx++ ] += y * lGain >> 0xF;
-				mixBuffer[ outIdx++ ] += y * rGain >> 0xF;
-				samFra += step;
-				samIdx += samFra >> 0xF;
-				samFra &= 0x7FFF;
+				var x = samIdx | 0;
+				var c = sampleData[ x ];
+				var m = sampleData[ x + 1 ] - c;
+				var y = ( m * ( samIdx - x ) ) + c;
+				outBuf[ outIdx++ ] += y * lGain;
+				outBuf[ outIdx++ ] += y * rGain;
+				samIdx += step;
 			}
 		} else {
 			while( outIdx < outEnd ) {
 				if( samIdx >= loopEnd ) {
-					if( loopLen < 2 ) break;
+					if( loopLen <= 1 ) break;
 					while( samIdx >= loopEnd ) samIdx -= loopLen;
 				}
-				var y = sampleData[ samIdx ];
-				mixBuffer[ outIdx++ ] += y * lGain >> 0xF;
-				mixBuffer[ outIdx++ ] += y * rGain >> 0xF;
-				samFra += step;
-				samIdx += samFra >> 0xF;
-				samFra &= 0x7FFF;
+				var y = sampleData[ samIdx | 0 ];
+				outBuf[ outIdx++ ] += y * lGain;
+				outBuf[ outIdx++ ] += y * rGain;
+				samIdx += step;
 			}
 		}
 	}
 	this.updateSampleIdx = function( count, sampleRate ) {
-		var step = ( ( freq << ( 0xF - 3 ) ) / ( sampleRate >> 3 ) ) | 0;
-		sampleFra += step * count;
-		sampleIdx += sampleFra >> 0xF;
-		var loopStart = sample.loopStart;
-		var loopLength = sample.loopLength;
-		var loopOffset = sampleIdx - loopStart;
-		if( loopOffset > 0 ) {
-			sampleIdx = loopStart;
-			if( loopLength > 1 ) sampleIdx += ( loopOffset % loopLength ) | 0;
+		sampleIdx += ( freq / sampleRate ) * count;
+		if( sampleIdx > sample.loopStart ) {
+			if( sample.loopLength > 1 ) {
+				sampleIdx = sample.loopStart + ( sampleIdx - sample.loopStart ) % sample.loopLength;
+			} else {
+				sampleIdx = sample.loopStart;
+			}
 		}
-		sampleFra &= 0x7FFF;
 	}
 	this.row = function( note ) {
 		noteKey = note.key;
@@ -607,7 +603,7 @@ function IBXMChannel( replay, id ) {
 			var rate = instrument.vibratoRate & 0x7F;
 			var type = instrument.vibratoType;
 			if( autoVibratoCount < sweep ) depth = depth * autoVibratoCount / sweep;
-			vibratoAdd += replay.module.waveform( autoVibratoCount * rate >> 2, type + 4 ) * depth >> 8;
+			vibratoAdd += waveform( autoVibratoCount * rate >> 2, type + 4 ) * depth >> 8;
 			autoVibratoCount++;
 		}
 	}
@@ -665,10 +661,33 @@ function IBXMChannel( replay, id ) {
 		}
 	}
 	var vibrato = function( fine ) {
-		vibratoAdd = replay.module.waveform( vibratoPhase, vibratoType & 0x3 ) * vibratoDepth >> ( fine ? 7 : 5 );
+		vibratoAdd = waveform( vibratoPhase, vibratoType & 0x3 ) * vibratoDepth >> ( fine ? 7 : 5 );
 	}
 	var tremolo = function() {
-		tremoloAdd = replay.module.waveform( tremoloPhase, tremoloType & 0x3 ) * tremoloDepth >> 6;
+		tremoloAdd = waveform( tremoloPhase, tremoloType & 0x3 ) * tremoloDepth >> 6;
+	}
+	var waveform = function( phase, type ) {
+		var amplitude = 0;
+		switch( type ) {
+			default: /* Sine. */
+				amplitude = sineTable[ phase & 0x1F ];
+				if( ( phase & 0x20 ) > 0 ) amplitude = -amplitude;
+				break;
+			case 6: /* Saw Up.*/
+				amplitude = ( ( ( phase + 0x20 ) & 0x3F ) << 3 ) - 255;
+				break;
+			case 1: case 7: /* Saw Down. */
+				amplitude = 255 - ( ( ( phase + 0x20 ) & 0x3F ) << 3 );
+				break;
+			case 2: case 5: /* Square. */
+				amplitude = ( phase & 0x20 ) > 0 ? 255 : -255;
+				break;
+			case 3: case 8: /* Random. */
+				amplitude = ( randomSeed >> 20 ) - 255;
+				randomSeed = ( randomSeed * 65 + 17 ) & 0x1FFFFFFF;
+				break;
+		}
+		return amplitude;
 	}
 	var tremor = function() {
 		if( retrigCount >= tremorOnTicks ) tremoloAdd = -64;
@@ -700,7 +719,16 @@ function IBXMChannel( replay, id ) {
 		}
 	}
 	var calculateFrequency = function() {
-		freq = replay.module.periodToFreq( period + vibratoAdd, arpeggioAdd );
+		var per = period + vibratoAdd;
+		if( replay.module.linearPeriods ) {
+			per = per - ( arpeggioAdd << 6 );
+			if( per < 28 || per > 7680 ) per = 7680;
+			freq = Math.round( replay.module.c2Rate * Math.pow( 2, ( 4608 - per ) / 768 ) );
+		} else {
+			per = per / Math.pow( 2, arpeggioAdd / 12 );
+			if( per < 28 ) per = 29021;
+			freq = Math.round( replay.module.c2Rate * 1712 / per );
+		}
 	}
 	var calculateAmplitude = function() {
 		var envVol = keyOn ? 64 : 0;
@@ -709,14 +737,13 @@ function IBXMChannel( replay, id ) {
 		var vol = volume + tremoloAdd;
 		if( vol > 64 ) vol = 64;
 		if( vol < 0 ) vol = 0;
-		vol = ( vol * replay.module.gain * 0x8000 ) >> 13;
-		vol = ( vol * fadeOutVol ) >> 15;
-		ampl = ( vol * replay.globalVol * envVol ) >> 12;
+		vol = vol * fadeOutVol / 0x8000;
+		ampl = vol * envVol * replay.globalVol * replay.module.gain / 0x2000000;
 		var envPan = 32;
 		if( instrument.panningEnvelope.enabled )
 			envPan = instrument.panningEnvelope.calculateAmpl( panEnvTick );
 		var panRange = ( panning < 128 ) ? panning : ( 255 - panning );
-		pann = panning + ( panRange * ( envPan - 32 ) >> 5 );
+		pann = ( panning + ( panRange * ( envPan - 32 ) >> 5 ) ) / 255;
 	}
 	var trigger = function() {
 		if( noteIns > 0 && noteIns <= replay.module.numInstruments ) {
@@ -774,9 +801,13 @@ function IBXMChannel( replay, id ) {
 				var key = noteKey + sample.relNote;
 				if( key < 1 ) key = 1;
 				if( key > 120 ) key = 120;
-				var per = replay.module.keyToPeriod( key, fineTune );
-				per = replay.module.c2Rate * per * 2 / sample.c2Rate;
-				portaPeriod = ( per >> 1 ) + ( per & 1 );
+				var per = ( key << 6 ) + ( fineTune >> 1 );
+				if( replay.module.linearPeriods ) {
+					per = 7744 - per;
+				} else {
+					per = Math.round( 29021 * Math.pow( 2, -per / 768 ) );
+				}
+				portaPeriod = Math.round( replay.module.c2Rate * per / sample.c2Rate );
 				if( !isPorta ) {
 					period = portaPeriod;
 					sampleIdx = sampleOffset;
@@ -850,7 +881,7 @@ function IBXMEnvelope() {
 			var dt = this.pointsTick[ point + 1 ] - this.pointsTick[ point ];
 			var da = this.pointsAmpl[ point + 1 ] - this.pointsAmpl[ point ];
 			ampl = this.pointsAmpl[ point ];
-			ampl += ( ( da << 24 ) / dt ) * ( tick - this.pointsTick[ point ] ) >> 24;
+			ampl += ( da / dt ) * ( tick - this.pointsTick[ point ] );
 		}
 		return ampl;
 	}
@@ -987,117 +1018,6 @@ function IBXMModule( moduleData ) {
 	this.sequence = new Int32Array( 1 );
 	this.patterns = [ new IBXMPattern( 4, 64 ) ];
 	this.instruments = [ new IBXMInstrument(), new IBXMInstrument() ];
-	this.periodTable = new Int16Array([
-		/* Periods for keys 0 to 15 with 8 finetune values. */
-		29021, 28812, 28605, 28399, 28195, 27992, 27790, 27590,
-		27392, 27195, 26999, 26805, 26612, 26421, 26231, 26042,
-		25855, 25669, 25484, 25301, 25119, 24938, 24758, 24580,
-		24403, 24228, 24054, 23881, 23709, 23538, 23369, 23201,
-		23034, 22868, 22704, 22540, 22378, 22217, 22057, 21899,
-		21741, 21585, 21429, 21275, 21122, 20970, 20819, 20670,
-		20521, 20373, 20227, 20081, 19937, 19793, 19651, 19509,
-		19369, 19230, 19091, 18954, 18818, 18682, 18548, 18414,
-		18282, 18150, 18020, 17890, 17762, 17634, 17507, 17381,
-		17256, 17132, 17008, 16886, 16765, 16644, 16524, 16405,
-		16287, 16170, 16054, 15938, 15824, 15710, 15597, 15485,
-		15373, 15263, 15153, 15044, 14936, 14828, 14721, 14616,
-		14510, 14406, 14302, 14199, 14097, 13996, 13895, 13795,
-		13696, 13597, 13500, 13403, 13306, 13210, 13115, 13021,
-		12927, 12834, 12742, 12650, 12559, 12469, 12379, 12290,
-		12202, 12114, 12027, 11940, 11854, 11769, 11684, 11600
-	]);
-	this.freqTable = new Int32Array([
-		/* Frequency for keys 109 to 121 with 8 fractional values. */
-		267616, 269555, 271509, 273476, 275458, 277454, 279464, 281489,
-		283529, 285584, 287653, 289738, 291837, 293952, 296082, 298228,
-		300389, 302566, 304758, 306966, 309191, 311431, 313688, 315961,
-		318251, 320557, 322880, 325220, 327576, 329950, 332341, 334749,
-		337175, 339618, 342079, 344558, 347055, 349570, 352103, 354655,
-		357225, 359813, 362420, 365047, 367692, 370356, 373040, 375743,
-		378466, 381209, 383971, 386754, 389556, 392379, 395222, 398086,
-		400971, 403877, 406803, 409751, 412720, 415711, 418723, 421758,
-		424814, 427892, 430993, 434116, 437262, 440430, 443622, 446837,
-		450075, 453336, 456621, 459930, 463263, 466620, 470001, 473407,
-		476838, 480293, 483773, 487279, 490810, 494367, 497949, 501557,
-		505192, 508853, 512540, 516254, 519995, 523763, 527558, 531381,
-		535232, 539111, 543017, 546952, 550915, 554908, 558929, 562979
-	]);
-	this.sineTable = new Int16Array([
-		   0,  24,  49,  74,  97, 120, 141, 161, 180, 197, 212, 224, 235, 244, 250, 253,
-		 255, 253, 250, 244, 235, 224, 212, 197, 180, 161, 141, 120,  97,  74,  49,  24
-	]);
-	this.keyToPeriod = function( key, fineTune ) {
-		if( this.linearPeriods ) {
-			return 7744 - ( key << 6 ) - ( fineTune >> 1 );
-		} else {
-			var tone = ( key << 6 ) + ( fineTune >> 1 );
-			var i = ( tone >> 3 ) % 96;
-			var c = this.periodTable[ i ] * 2;
-			var m = this.periodTable[ i + 1 ] * 2 - c;
-			var x = tone & 0x7;
-			var y = ( ( ( m * x ) >> 3 ) + c ) >> ( ( tone / 768 ) | 0 );
-			return ( y >> 1 ) + ( y & 1 );
-		}
-	}
-	this.periodToKey = function( period ) {
-		var key = 0, oct = 0;
-		while( period < this.periodTable[ 96 ] ) {
-			period = period << 1;
-			oct++;
-		}
-		while( key < 12 ) {
-			var d1 = this.periodTable[ key << 3 ] - period;
-			var d2 = period - this.periodTable[ ( key + 1 ) << 3 ];
-			if( d2 >= 0 ) {
-				if( d2 < d1 ) key++;
-				break;
-			}
-			key++;
-		}
-		return oct * 12 + key;
-	}
-	this.periodToFreq = function( period, keyAdd ) {
-		if( this.linearPeriods ) {
-			period = period - ( keyAdd << 6 );
-			if( period < 28 || period > 7680 ) period = 7680;
-			var tone = 7680 - period;
-			var i = ( tone >> 3 ) % 96;
-			var c = this.freqTable[ i ];
-			var m = this.freqTable[ i + 1 ] - c;
-			var x = tone & 0x7;
-			var y = ( ( m * x ) >> 3 ) + c;
-			return y >> ( 9 - ( ( tone / 768 ) | 0 ) );
-		} else {
-			period = period * ( this.periodTable[ ( keyAdd & 0xF ) << 3 ] << 1 ) / this.periodTable[ 0 ];
-			period = ( period >> 1 ) + ( period & 1 );
-			if( period < 28 ) period = this.periodTable[ 0 ];
-			return ( this.c2Rate * 1712 / period ) | 0;
-		}
-	}
-	this.waveform = function( phase, type ) {
-		var amplitude = 0;
-		switch( type ) {
-			default: /* Sine. */
-				amplitude = this.sineTable[ phase & 0x1F ];
-				if( ( phase & 0x20 ) > 0 ) amplitude = -amplitude;
-				break;
-			case 6: /* Saw Up.*/
-				amplitude = ( ( ( phase + 0x20 ) & 0x3F ) << 3 ) - 255;
-				break;
-			case 1: case 7: /* Saw Down. */
-				amplitude = 255 - ( ( ( phase + 0x20 ) & 0x3F ) << 3 );
-				break;
-			case 2: case 5: /* Square. */
-				amplitude = ( phase & 0x20 ) > 0 ? 255 : -255;
-				break;
-			/* Random.
-			case 3: case 8: 
-				amplitude = ( randomSeed >> 20 ) - 255;
-				randomSeed = ( randomSeed * 65 + 17 ) & 0x1FFFFFFF;
-				break;*/
-		}
-		return amplitude;
-	}
 	this.loadXM = function( moduleData ) {
 		if( moduleData.uleShort( 58 ) != 0x0104 )
 			throw "XM format version must be 0x0104!";
@@ -1414,9 +1334,10 @@ function IBXMModule( moduleData ) {
 		for( var patIdx = 0; patIdx < this.numPatterns; patIdx++ ) {
 			var pattern = this.patterns[ patIdx ] = new IBXMPattern( this.numChannels, 64 );
 			for( var patDataIdx = 0; patDataIdx < pattern.data.length; patDataIdx += 5 ) {
-				var period = ( moduleData.uByte( moduleDataIdx ) & 0xF ) << 8;
-				period = ( period | moduleData.uByte( moduleDataIdx + 1 ) ) * 4;
-				if( period > 112 ) pattern.data[ patDataIdx ] = this.periodToKey( period );
+				var key = ( moduleData.uByte( moduleDataIdx ) & 0xF ) << 8;
+				key = ( key | moduleData.uByte( moduleDataIdx + 1 ) ) * 4;
+				key = Math.round( -12 * Math.log( key / 29021 ) / Math.log( 2 ) );
+				if( key < 97 ) pattern.data[ patDataIdx ] = key;
 				var ins = ( moduleData.uByte( moduleDataIdx + 2 ) & 0xF0 ) >> 4;
 				ins = ins | moduleData.uByte( moduleDataIdx ) & 0x10;
 				pattern.data[ patDataIdx + 1 ] = ins;
